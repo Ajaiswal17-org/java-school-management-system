2fd88fcf4518a6508983c42aa11eb5d0609a99b7
Ajaiswal17-org <68050101+Ajaiswal17-org@users.noreply.github.com>
2021-01-04 15:25:21 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,37 @@
+package sms2point0;
+
+import java.util.*;
+public class Main
+{
+    public void main()
+    {
+        Login.fnStudent();
+        
+    }
+}
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    
